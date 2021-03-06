d72b1e2110a6295fbeec8572295f4bb12b2341a2
nitin reddy <reddynitin3@gmail.com>
2021-03-06 22:23:57 +0530

    Got hold of the classes

--- classes/example_2_inheritance_class.js
+++ classes/example_2_inheritance_class.js
@@ -1,35 +1,60 @@
-class Animal {
-  constructor(name, speed) {
-    this.name = name;
-    this.speed = speed;
+// class Animal {
+//   constructor(name, speed) {
+//     this.name = name;
+//     this.speed = speed;
+//   }
+
+//   getDetails() {
+//     return `${this.name} runs at ${this.speed}km/hr`
+//   }
+// }
+
+// class Rabbit extends Animal {
+//   constructor(name, speed, food) {
+//     super(name, speed);
+//     this.food = food;
+//   }
+
+//   getDetails() {
+//     return `${this.name} runs at ${this.speed}km/hr after eating ${this.food}`
+//   }
+
+//   eats() {
+//     return `${this.name} eats ${this.food}`
+//   }
+// }
+
+
+// var rabbit1 = new Rabbit("Bunny", 10, 'grass')
+
+// var rabbit2 = new Rabbit("Sunny", 30, 'leafs')
+// console.log(rabbit1.eats())
+// console.log(rabbit1.getDetails())
+
+// console.log(rabbit2.eats())
+// console.log(rabbit2.getDetails())
+
+class CoffeeMachine {
+  #waterLimit = 0;
+
+  #fixWaterAmount(value) {
+    if (value < 0) return 0;
+    if (value > this.#waterLimit) return value;
   }
 
-  getDetails() {
-    return `${this.name} runs at ${this.speed}km/hr`
+  setWaterAmount(value) {
+    this.#waterLimit = this.#fixWaterAmount(value);
   }
+
+  getWaterAmount() {
+    return this.#waterLimit;
+  }
+
 }
 
-class Rabbit extends Animal {
-  constructor(name, speed, food) {
-    super(name, speed);
-    this.food = food;
-  }
+const coffee = new CoffeeMachine()
 
-  getDetails() {
-    return `${this.name} runs at ${this.speed}km/hr after eating ${this.food}`
-  }
-
-  eats() {
-    return `${this.name} eats ${this.food}`
-  }
-}
-
-
-var rabbit1 = new Rabbit("Bunny", 10, 'grass')
-
-var rabbit2 = new Rabbit("Sunny", 30, 'leafs')
-console.log(rabbit1.eats())
-console.log(rabbit1.getDetails())
-
-console.log(rabbit2.eats())
-console.log(rabbit2.getDetails())
+coffee.setWaterAmount(200)
+console.log(coffee.getWaterAmount())
+coffee.setWaterAmount(300)
+console.log(coffee.getWaterAmount())
